1b655a9c130a237f8eac338bb1b86627c378a289
Eugenio Díaz López <114876251+redporkins@users.noreply.github.com>
2026-01-27 09:16:58 -0600

    Minor fixes on all code

--- SQL/ventas_llamadas.sql
+++ SQL/ventas_llamadas.sql
@@ -1,14 +1,20 @@
 SELECT  
-	vr.Costo,
-	vr.NoRGU,
-	vr.CreatorUserId,
-	vr.CreationTime
--- SUM(vr.Costo) AS MontoVentas,
--- SUM(vr.NoRGU) AS CantidadRGU,
--- COUNT(vr.CreatorUserId) AS RecuentoVentas
+	* 
+	-- vr.Costo,
+	-- vr.NoRGU,
+	-- vr.CreatorUserId,
+	-- vr.CreationTime
+	-- SUM(vr.Costo) AS MontoVentas,
+	-- AVG(vr.Costo) AS PromedioVenta,
+	-- SUM(vr.CostoComplemento) AS MontoComplemento,
+	-- AVG(vr.CostoComplemento) AS PromedioComplemento,
+	-- SUM(vr.NoRGU) AS CantidadRGU,
+	-- AVG(vr.NoRGU) AS PromedioRGU,
+	-- COUNT(vr.Id) AS CantidadVentas,
+	-- COUNT(vr.CreatorUserId) AS RecuentoVentas
 FROM   OnecontactDb.Venta.VentasRegistradas vr 
 WHERE 
 	vr.CreationTime BETWEEN '2026-01-01' AND '2026-12-31'
-	AND vr.VentaOrigen = 3
-    ORDER BY vr.CreationTime DESC; 
+	-- AND vr.VentaOrigen = 3
+    -- ORDER BY vr.CreationTime DESC; 
 	
